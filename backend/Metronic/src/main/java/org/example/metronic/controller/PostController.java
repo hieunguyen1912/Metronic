2fa44a626e2b7849d75af99b7a8d140136fc076e
HieuNguyeen <hn151331@gmail.com>
2025-12-16 14:12:03 +0700

    fix

--- backend/Metronic/src/main/java/org/example/metronic/controller/PostController.java
+++ backend/Metronic/src/main/java/org/example/metronic/controller/PostController.java
@@ -5,27 +5,41 @@ import org.example.metronic.dto.PageResponse;
 import org.example.metronic.dto.PostDto;
 import org.example.metronic.dto.PostMockData;
 import org.example.metronic.util.PaginationUtil;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequestMapping("/posts")
-@RestController
+@RequestMapping("admin/posts")
+@Controller
 public class PostController {
 
     @PostMapping
-    public ResponseEntity<PostDto> post(@Valid @RequestBody PostDto postDto) {
+    public String post(@Valid @ModelAttribute("post") PostDto postDto,
+                       BindingResult result,
+                       Model model
+    ) {
+        if (result.hasErrors()) {
+            return "create_post";
+        }
         postDto.setId(System.currentTimeMillis());
-        return ResponseEntity.status(HttpStatus.CREATED).body(postDto);
+        return "redirect:/admin/posts";
     }
 
-    @GetMapping
-    public PageResponse<PostDto> getAllPosts(@RequestParam(value = "page", defaultValue = "1") int page,
+    @GetMapping("/create")
+    public String createPost(Model model) {
+        model.addAttribute("post", new PostDto());
+        return "create_post";
+    }
+
+    @GetMapping("/all")
+    public String getAllPosts(@RequestParam(value = "page", defaultValue = "1") int page,
                                              @RequestParam(value = "size", defaultValue = "5") int size,
                                              @RequestParam(value = "search", required = false) String search,
-                                             @RequestParam(value = "status", required = false) String status) {
+                                             @RequestParam(value = "status", required = false) String status,
+                                             Model model) {
         List<PostDto> allPosts = PostMockData.getMockPosts();
 
         if (search != null && !search.trim().isEmpty()) {
@@ -39,11 +53,12 @@ public class PostController {
         List<PostDto> data = PaginationUtil.paginate(allPosts, page, size);
         int totalPages = (int) Math.ceil((double) allPosts.size() / size);
 
-        return PageResponse.<PostDto>builder()
-                .data(data)
-                .page(page)
-                .size(size)
-                .totalElements(allPosts.size())
-                .totalPages(totalPages).build();
+        model.addAttribute("posts", data);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", totalPages);
+        model.addAttribute("search", search);      // ✅ LƯU SEARCH
+        model.addAttribute("status", status);
+
+        return "all_post";
     }
 }
